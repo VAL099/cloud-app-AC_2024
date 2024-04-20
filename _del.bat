90250a2d81ca5f9888973a919ef063acd1eadeb4
VAL099 <valeriy099@yahoo.com>
2024-04-20 11:43:44 +0300

    First

---
+++ _del.bat
@@ -0,0 +1,3 @@
+kubectl delete deployments --all
+kubectl delete pods --all 
+kubectl delete service ac-laboratory-service
---
+++ test.py
@@ -0,0 +1,7 @@
+"""
+-> Pipeline for everithing (CI\CD, rollback, build)
+-> No DownTime
+-> 
+-> 
+-> 
+"""
